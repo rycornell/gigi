e242a14ff463fc8ea1835a409b0ff2a46174c873
Ryan Manns <rmanns@appriver.com>
2015-03-31 08:39:30 -0400

    get json data instead of parsing DOM

--- src/calc.js
+++ src/calc.js
@@ -1,66 +1,113 @@
+function loadAllEffort() {
 
-function loadAllEffort(){
+    // ajax load this url and parse DOM
+    var baseUrl = '';
+    var backlogJsonUrl = baseUrl + '';
+    var sprintUrl = baseUrl + '';
 
-  sendMessage(function(response){
-    console.log(response);
+    chrome.tabs.getSelected(null, function(tab) {
+        var tabUrl = tab.url;
 
-    var data = response;
+        loadHtmlData(tabUrl, function(data) {
+            calculateEffort('Sprint ' + tabUrl.slice(-2), data);
+        });
+    });
 
-    var done = sumEffort(data, 'Done');
-    var committed = sumEffort(data, 'Committed');
-    var approved = sumEffort(data, 'Approved');
+    loadJsonData(backlogJsonUrl, function(data) {
+        calculateEffort('Backlog', data);
+    });
+}
+
+function loadHtmlData(url, callback) {
+    $.get(url, function(data) {
+
+        var html = $(data);
+        var grid = html.find('div.productbacklog-grid-results');
+        var script = grid.find('script.options');
+        var scriptText = script.text();
+
+        var backlogData = JSON.parse(scriptText);
+        callback(backlogData);
+    });
+}
+
+function loadJsonData(url, callback) {
+    $.get(url, function(data) {
+        callback(data.queryResults);
+    });
+}
+
+function calculateEffort(title, data) {
+    var stateColumnIndex = findColumn(/.*State$/i, data.pageColumns);
+    var effortColumnIndex = findColumn(/.*Effort$/i, data.pageColumns);
+
+    var done = 0;
+    var committed = 0;
+    var approved = 0;
+
+    for (var i = 0; i < data.payload.rows.length; i++) {
+        var row = data.payload.rows[i];
+        var state = row[stateColumnIndex];
+        var effort = parseInt(row[effortColumnIndex]);
+
+        if (isNaN(effort)) continue;
+
+        switch (state) {
+            case 'Done':
+                done += effort;
+                break;
+
+            case 'Committed':
+                committed += effort;
+                break;
+
+            case 'Approved':
+                approved += effort;
+                break;
+        }
+    }
+
     var total = done + committed + approved;
 
-    renderTotal(total);
-    renderDone(done);
-    renderCommitted(committed);
-    renderApproved(approved);
-  });
+    appendEffort(title, total, done, committed, approved);
 }
 
-function sendMessage(callback) {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    lastTabId = tabs[0].id;
-    chrome.tabs.sendMessage(lastTabId, "Background page started.", callback);
-  });
+function findColumn(labelRe, columns) {
+    for (var i = 0; i < columns.length; i++) {
+        if (labelRe.test(columns[i])) {
+            return i;
+        }
+    }
 }
 
-function sumEffort(d, stateName){
-  var sum = 0;
+function appendEffort(title, total, done, committed, approved) {
+    var html = [];
+    html.push('<h4>' + title + '</h4>');    
+    html.push('<table>');
+    html.push('<tbody>');
+    html.push('<tr>');
+    html.push('<td class="label">Done</td>');
+    html.push('<td class="effort"><span>' + done + '</span></td>');
+    html.push('</tr>');
+    html.push('<tr>');
+    html.push('<td class="label">Committed</td>');
+    html.push('<td class="effort"><span>' + committed + '</span></td>');
+    html.push('</tr>');
+    html.push('<tr>');
+    html.push('<td class="label">Approved</td>');
+    html.push('<td class="effort"><span>' + approved + '</span></td>');
+    html.push('</tr>');
+    html.push('<tr class="last">');
+    html.push('<td class="label">Total</td>');
+    html.push('<td class="effort"><span>' + total + '</span></td>');
+    html.push('</tr>');
+    html.push('</tbody>');
+    html.push('</table>');
+    html.push('<hr>');
 
-  d.data.forEach(function(e){
-    var state = e.state;
-    var val = parseInt(e.effort);
-
-    if (state === stateName && !isNaN(val)){
-      console.log('adding ' + val);
-      sum += val;
-    }    
-  });
-
-  return sum;
-}
-
-function renderTotal(effort) {
-  var el = document.querySelector('#totalEffort');
-  el.innerHTML = effort;
-}
-
-function renderDone(effort) {
-  var el = document.querySelector('#doneEffort');
-  el.innerHTML = effort;
-}
-
-function renderCommitted(effort) {
-  var el = document.querySelector('#committedEffort');
-  el.innerHTML = effort;
-}
-
-function renderApproved(effort) {
-  var el = document.querySelector('#approvedEffort');
-  el.innerHTML = effort;
+    $('body').append(html.join('')).slideDown();
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  loadAllEffort();
+    loadAllEffort();
 });
